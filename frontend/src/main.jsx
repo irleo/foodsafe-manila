4e1e52500f0b40f06334b3f0c9f8765e409ba19b
irleo <leomargarzon29@gmail.com>
2026-05-31 22:52:00 +0800

    production

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import "leaflet/dist/leaflet.css";
 
 import App from './App.jsx'
 
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || "";
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
